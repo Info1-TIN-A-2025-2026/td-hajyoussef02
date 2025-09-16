fe65c4fb2c887d824c7be8098a0b9de588c53267
hajyoussef02 <mohamed.hajyoussef@heig-vd.ch>
2025-09-16 12:34:48 +0000

    add TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23 ;
+	int account_value = -42 ;
+	float temp = 18.2 ;
+	double pi = 3.141592653589793 ;
+	
+}
